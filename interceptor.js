c5d43ae4223c6e3a3098a09761ac3a5706a21235
shenxin <botzgbj@gmail.com>
2023-04-10 13:37:39 +0800

    留下有用的拦截代码

--- interceptor.js
+++ interceptor.js
@@ -1,9 +1,8 @@
 import  "./ajaxhook.min.js";
-// import { proxy } from "ajax-hook";
 ah.proxy({
   //请求发起前进入
   onRequest: (config, handler) => {
-    console.log(config.url)
+    console.log(config.url,3)
     handler.next(config);
   },
   //请求发生错误时进入，比如超时；注意，不包括http状态码错误，如404仍然会认为请求成功880
